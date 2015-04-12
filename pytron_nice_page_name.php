181ab1c781541ed318eb7593785e62340dac3bc2
Stan Lee Switaj <BehemothIncCEO@gmail.com>
2015-04-12 16:30:01 -0400

    small edit to have the same code base as pyThor for include files

--- pytron_nice_page_name.php
+++ pytron_nice_page_name.php
@@ -777,10 +777,9 @@ import sys
 
 # INCLUDES TO BE PLACED HERE
 file_to_include = 'include.py'
-# including this way due to execfile does not include a file from within a def,function
-#execfile(include_quick_tags_file(file_to_include))	# this function used to include each python file		 
+#execfile(include_quick_tags_file(file_to_include)) # including this way due to execfile does not include file from within a def,function
 
-		
+
 def source_code():    # note, this is to display whatever source code is within this function
 	
 	return <%
